dbab3a92afd13a07dff125c493c6bdd0d97ba5a9
Meetbhuva7 <152474014+Meetbhuva7@users.noreply.github.com>
2023-12-19 15:21:39 +0530

    Add files via upload

---
+++ loop.c/work-10.c
@@ -0,0 +1,13 @@
+//print the multification table of n
+#include<stdio.h>
+int main(){
+    int i=1,n;
+    printf("enter no:");
+    scanf("%d",&n);
+    while (i<=10)
+    {
+        printf("\n %d * %d =%d",n,i,n*i);
+        i++;
+    }
+    return 0;
+}
---
+++ loop.c/work-9.c
@@ -0,0 +1,15 @@
+//find the factorial of n using
+#include<stdio.h>
+int main(){
+    int i=1,n,mult=1;
+    printf("\n enter input number.");
+    scanf("%d",&n);
+    while (i<=n)
+    {
+        mult*=i;
+        i++;
+    }
+    printf("%d \n",mult);
+
+    return 0;
+}
