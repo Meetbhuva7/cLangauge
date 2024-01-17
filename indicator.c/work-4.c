1541f359be6f581cb2d7f9cb70b5e11494c44bb5
Meetbhuva7 <152474014+Meetbhuva7@users.noreply.github.com>
2024-01-17 21:49:20 +0530

    Add files via upload

---
+++ indicator.c/work-4.c
@@ -0,0 +1,25 @@
+// find length of string using pointer
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+
+    char str[100], *ptr;
+    int count = 0;
+
+    printf("enetr name = ");
+    gets(str);
+    printf("name is = ");
+    puts(str);
+
+    ptr = str;
+    while (*ptr != '\0')
+    {
+        count++;
+        ptr++;
+    }
+    printf("string length is: %d", count);
+
+    return 0;
+}
